8ab68afac482d77827dae358036820feec124c69
SURBHI JAIN <121885461+Surbhi399@users.noreply.github.com>
2025-07-20 16:35:51 +0530

    Update Entry.jsx

--- src/Components/Entry.jsx
+++ src/Components/Entry.jsx
@@ -11,7 +11,7 @@ export default function Entry(props) {
             <div className="info-container">
                 <img 
                     className="marker"
-                    src="../images/marker.png" 
+                    src="public/images/marker.png" 
                     alt="map marker icon"
                 />
                 <span className="country">India</span>
